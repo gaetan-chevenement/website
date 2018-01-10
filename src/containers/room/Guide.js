4b603740f9b00500c1fe89c2cb11c7096898ab28
Frédéric Langlade-Bellone <fred@ioweb.fr>
2018-01-10 12:28:56 +0100

    integration iteration 3

--- src/containers/room/Guide.js
+++ src/containers/room/Guide.js
@@ -3,19 +3,21 @@ import style from './style.css';
 const Guide = () => (
   <div className={style.guide}>
     <div>
-      <h2 className="text-center">Notre guide logement offert</h2>
-      <div className={style.guideFeatures}>
-        <ul>
+      <h2>Notre guide logement offert</h2>
+      <div className="grid-2">
+        <ul className="one-half">
           <li>Où chercher son logement</li>
           <li>Agences, résidences...</li>
         </ul>
-        <ul>
+        <ul className="one-half">
           <li>Les pièges à éviter</li>
           <li>Les frais à prévoir</li>
         </ul>
       </div>
       <div>
-        <a href="/">Je télécharge mon Guide Logement</a>
+        <a href="/" className={style.downloadGuideBtn}>
+          Je télécharge mon Guide Logement
+        </a>
       </div>
     </div>
   </div>
