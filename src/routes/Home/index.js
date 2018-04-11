31476224773b1ee83bd39676bf4667e695abf03f
Louis-Rémi Babé <lrbabe@gmail.com>
2018-04-11 17:49:30 +0200

    Update index.js

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -56,8 +56,10 @@ export default function Home({ lang }) {
 const definition = { 'fr-FR': {
   search: {
     title: 'Votre colocation prête à vivre',
-    subtitle: 'Trouvez aujourd'\'hui votre colocation meublée, équipée,
-                tout inclus, en plein centre.',
+    subtitle: `
+      Trouvez aujourd'hui votre colocation meublée, équipée,
+      tout inclus, en plein centre.
+    `,
   },
   cities: 'Découvrez les villes Chez Nestor'
   services: 'Une nouvelle expérience de la colocation'
