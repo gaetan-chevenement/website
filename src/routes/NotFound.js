4568add282a2730739ae3d2bec52b8c1b42c40f1
Frédéric Langlade-Bellone <fred@parapluie.org>
2018-07-25 11:35:47 +0200

    wordpress 404 page

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -1,31 +1,8 @@
-import { PureComponent } from 'preact-compat';
 import { h } from 'preact';
-import { IntlProvider, Text } from 'preact-i18n';
+import Page from './Page';
 
 export default function NotFound({ lang }) {
   return (
-    <IntlProvider definition={definition[lang]}>
-      <div className="content notfound-content">
-        <h1 className="notfound-title">
-          <Text id="title">Oops!</Text>
-        </h1>
-        <p>
-          <Text id="description">We can't seem to find the page you're looking for.</Text>
-        </p>
-        <p>
-          <a href="/">
-            <Text id="link">
-              While you're here, why not have a look at our offers ?
-            </Text>
-          </a>
-        </p>
-      </div>
-    </IntlProvider>
+    <Page slug={'404'} lang={lang} noRedirect />
   );
 }
-
-const definition = { 'fr-FR': {
-  title: 'Oups!',
-  description: 'La page que vous recherchez semble introuvable.',
-  link: 'Tant que vous êtes ici, pourquoi ne pas jeter un oeil à nos offres ? ',
-} };
