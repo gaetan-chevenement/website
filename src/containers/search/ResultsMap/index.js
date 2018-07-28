05bbb497d736c3f9058663323a5b4ed26294910c
Frédéric Langlade-Bellone <fred@ioweb.fr>
2018-07-29 00:41:48 +0200

    not showing legend if title is empty

--- src/containers/search/ResultsMap/index.js
+++ src/containers/search/ResultsMap/index.js
@@ -1,40 +1,40 @@
-import { Component }          from 'react';
-import { connect }            from 'react-redux';
+import { Component } from 'react'
+import { connect } from 'react-redux'
 import {
   Map,
   TileLayer,
   Marker,
-  Popup,
-}                             from 'react-leaflet';
-import L                      from 'leaflet';
-import MarkerClusterGroup     from 'react-leaflet-markercluster';
-import filter                 from 'lodash/filter';
-import orderBy                from 'lodash/orderBy';
-import Room                   from '~/components/search/Room';
-import _const                 from '~/const';
-import Utils                  from '~/utils';
+  Popup
+} from 'react-leaflet'
+import L from 'leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
+import filter from 'lodash/filter'
+import orderBy from 'lodash/orderBy'
+import Room from '~/components/search/Room'
+import _const from '~/const'
+import Utils from '~/utils'
 
-import 'leaflet/dist/leaflet.css';
-import 'react-leaflet-markercluster/dist/styles.min.css';
+import 'leaflet/dist/leaflet.css'
+import 'react-leaflet-markercluster/dist/styles.min.css'
 
-const _ = { filter, orderBy };
-const { MAPBOX_TOKEN } = _const;
+const _ = { filter, orderBy }
+const { MAPBOX_TOKEN } = _const
 
 const DEFAULT_ICON = new L.Icon({
   iconUrl: require('~/assets/search/map-marker-default.png'),
-  iconSize: [45, 66],
-});
+  iconSize: [45, 66]
+})
 
 const HIGHLIGHT_ICON = new L.Icon({
   iconUrl: require('~/assets/search/map-marker-highlight.png'),
-  iconSize: [45, 66],
-});
+  iconSize: [45, 66]
+})
 
 // We only want to cluster at the street address level.
 // that should do the trick
-const maxClusterRadius = 1;
+const maxClusterRadius = 1
 
-function iconCreateFunction(cluster) {
+function iconCreateFunction (cluster) {
   return L.divIcon({
     className: 'map-rooms-cluster',
     html: (`
@@ -42,27 +42,26 @@ function iconCreateFunction(cluster) {
         <img src="${require('~/assets/search/map-marker-cluster.png')}" />
       </div>
     `),
-    iconSize: [45, 66],
-  });
+    iconSize: [45, 66]
+  })
 }
 
-const DEFAULT_BBOX = [[51.089062, 9.55932], [41.33374, -5.1406]];
+const DEFAULT_BBOX = [[51.089062, 9.55932], [41.33374, -5.1406]]
 const tileLayerUrl =
-  `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${MAPBOX_TOKEN}`;
+  `https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${MAPBOX_TOKEN}`
 
 class ResultsMap extends Component {
-
   // invalidate the size of the map every time it has been re-rendered
-  componentDidUpdate() {
-    if ( this._map ) {
-      this._map.leafletElement.invalidateSize();
+  componentDidUpdate () {
+    if (this._map) {
+      this._map.leafletElement.invalidateSize()
     }
   }
 
-  render({ lang, arrivalDate, highlightedRoomId, arrRooms }) {
-    const bounds = arrRooms.length > 0 ?
-      new L.LatLngBounds( arrRooms.map(({ latLng }) => (latLng)) ).pad(0.2) :
-      DEFAULT_BBOX;
+  render ({ lang, arrivalDate, highlightedRoomId, arrRooms }) {
+    const bounds = arrRooms.length > 0
+      ? new L.LatLngBounds(arrRooms.map(({ latLng }) => (latLng))).pad(0.2)
+      : DEFAULT_BBOX
 
     return (
       <Map
@@ -70,7 +69,7 @@ class ResultsMap extends Component {
           width: '100%',
           height: '100%',
           position: 'relative',
-          overflow: 'hidden',
+          overflow: 'hidden'
         }}
         bounds={bounds}
         scrollWheelZoom={false}
@@ -79,7 +78,7 @@ class ResultsMap extends Component {
       >
         <TileLayer
           url={tileLayerUrl}
-          id="mapbox.streets"
+          id='mapbox.streets'
           attribution={`
             © <a href="https://www.mapbox.com/about/maps/">Mapbox</a>
             © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>
@@ -93,7 +92,7 @@ class ResultsMap extends Component {
         />
         <MarkerClusterGroup {...{ maxClusterRadius, iconCreateFunction }}
           ref={(markerClusterGroup) => {
-            this.markerClusterGroup = markerClusterGroup;
+            this.markerClusterGroup = markerClusterGroup
           }}
         >
           { // Leaflet doesn't like it when we move Marker outside of this render
@@ -108,14 +107,15 @@ class ResultsMap extends Component {
               </Marker>
             ))
           }
+
         </MarkerClusterGroup>
       </Map>
-    );
+    )
   }
 }
 
 const mapStateToProps = (state, { hightlightedRoomId }) => {
-  const { route: { lang, date }, rooms, apartments } = state;
+  const { route: { lang, date }, rooms, apartments } = state
 
   return {
     lang,
@@ -129,10 +129,10 @@ const mapStateToProps = (state, { hightlightedRoomId }) => {
         pictures: [].concat(
           Utils.getPictures(room),
           Utils.getPictures(apartments[room.ApartmentId])
-        ),
+        )
       })),
-    hightlightedRoomId,
-  };
-};
+    hightlightedRoomId
+  }
+}
 
-export default connect(mapStateToProps)(ResultsMap);
+export default connect(mapStateToProps)(ResultsMap)
