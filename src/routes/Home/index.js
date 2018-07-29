27e123ccb3952e8a242f83a0f0c1081310215717
Frédéric Langlade-Bellone <fred@ioweb.fr>
2018-07-29 09:03:01 +0200

    two lines search form on home page

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,57 +1,57 @@
-import { IntlProvider, Text } from 'preact-i18n';
-import Utils                  from '~/utils';
-import Slideshow              from '~/components/home/Slideshow';
-import Cities                 from '~/components/home/Cities';
-import Services               from '~/components/home/Services';
-import Testimonies            from '~/components/home/Testimonies';
-import Guide                  from '~/components/home/Guide';
-import SearchForm             from '~/components/SearchForm';
-import style                  from './style.css';
+import { IntlProvider, Text } from 'preact-i18n'
+import Utils from '~/utils'
+import Slideshow from '~/components/home/Slideshow'
+import Cities from '~/components/home/Cities'
+import Services from '~/components/home/Services'
+import Testimonies from '~/components/home/Testimonies'
+import Guide from '~/components/home/Guide'
+import SearchForm from '~/components/SearchForm'
+import style from './style.css'
 
-function Home({ lang }) {
+function Home ({ lang }) {
   return (
     <IntlProvider definition={definition[lang]}>
-      <div class="home">
+      <div class='home'>
         <Slideshow>
           <div className={style.searchEngine}>
             <h1>
-              <Text id="search.title">Your ready-to-live-in flatshares</Text>
+              <Text id='search.title'>Your ready-to-live-in flatshares</Text>
             </h1>
             <h3>
-              <Text id="search.subtitle">
+              <Text id='search.subtitle'>
                 Find your furnished, fully equipped, all-inclusive flatshare,
                 in the heart of the city-centre today.
               </Text>
             </h3>
-            <SearchForm />
+            <SearchForm mode='home' />
           </div>
         </Slideshow>
-        <div class="content">
-          <h2 class="text-center">
-            <Text id="cities">Discover the cities with Chez Nestor</Text>
+        <div class='content'>
+          <h2 class='text-center'>
+            <Text id='cities'>Discover the cities with Chez Nestor</Text>
           </h2>
           <Cities />
         </div>
         <div>
-          <h2 class="text-center">
-            <Text id="services">A new way of flatsharing</Text>
+          <h2 class='text-center'>
+            <Text id='services'>A new way of flatsharing</Text>
           </h2>
           <Services />
         </div>
-        <div style="background: #E1E1E1; padding: 1px 0;">
-          <div class="content">
-            <h2 class="text-center">
-              <Text id="testimonies">Our housemates recommend us</Text>
+        <div style='background: #E1E1E1; padding: 1px 0;'>
+          <div class='content'>
+            <h2 class='text-center'>
+              <Text id='testimonies'>Our housemates recommend us</Text>
             </h2>
             <Testimonies />
           </div>
         </div>
-        <div class="content">
+        <div class='content'>
           <Guide />
         </div>
       </div>
     </IntlProvider>
-  );
+  )
 }
 
 const definition = { 'fr-FR': {
@@ -60,11 +60,11 @@ const definition = { 'fr-FR': {
     subtitle: `
       Trouvez aujourd'hui votre colocation meublée, équipée,
       tout inclus, en plein centre.
-    `,
+    `
   },
   cities: 'Découvrez les villes Chez Nestor',
   services: 'Une nouvelle expérience de la colocation',
-  testimonies: 'Nos colocataires nous recommandent',
-} };
+  testimonies: 'Nos colocataires nous recommandent'
+} }
 
-export default Utils.connectLang(Home);
+export default Utils.connectLang(Home)
