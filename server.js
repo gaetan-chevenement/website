0672f0dda2591f381e3474d2379d6dfa6e1af8b6
Louis-Rémi Babé <lrbabe@gmail.com>
2018-09-14 17:33:28 +0200

    Cleanup and optimize server.js

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const bundle            = require('./build/ssr-build/ssr-bundle');
 global.fetch = fetch;
 
 const App = bundle.default;
-const { defaultState, Helmet, configureStore } = App;
-const { PORT = 3001 } = process.env;
-// TODO: improve this?
-const template = readFileSync('build/index.html', 'utf8');
+const { defaultState, Helmet, configureStore, prefetchRoutes } = App;
+const { PORT = 3001, NODE_ENV } = process.env;
+const { style, scripts } = parseBuiltTemplate();
 const assetsExtensions = [
   '.jpg',
   '.jpeg',
@@ -22,55 +21,54 @@ const assetsExtensions = [
   '.map',
   '.svg',
 ];
-const scriptsRgx = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
 const app = express();
 
+// Static assets should only be served by this file in development
+// Cloudflare should take care of routing static assets requests to Netlify
+if ( NODE_ENV !== 'production' && NODE_ENV !== 'staging' ) {
+  app.use((req, res, next) => {
+    const isAsset = assetsExtensions.some((ext) =>
+      req.url.toLowerCase().endsWith(ext)
+    );
+
+    if ( isAsset ) {
+      return express.static('build')(req, res, next);
+    }
+
+    next();
+  });
+}
+
 app.use((req, res, next) => {
-  const isAsset = assetsExtensions.filter(
-    ext => req.url.toLowerCase().endsWith(ext)
-  ).length > 0;
-
-  if (isAsset) {
-    console.log('asset', req.url);
-    return express.static('build')(req, res, next);
-  }
-  console.log('page', req.url);
-
   const store = configureStore(defaultState);
 
-  App.prefetchRoutes(req.url, store.dispatch)
+  prefetchRoutes(req.url, store.dispatch)
     .then(() => {
-      let body = render(h(App, {
-        url: req.url,
-        store,
-      }));
+      const body = render(h(App, { url: req.url, store }));
+      const helmet = Helmet.rewind();
+      const state = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
 
-      const head = Helmet.rewind();
-      const style = template.match(/<link href="(.*)" rel="stylesheet">/)[1];
-      let scripts = [], out;
-      while ((out = scriptsRgx.exec(template)) != null) {
-        scripts.push(out[0]);
-      }
       const html = `
-    <!doctype html>
-    <html>
-        <head ${head.htmlAttributes.toString()}>
-            ${head.title.toString()}
-            ${head.meta.toString()}
-            ${head.link.toString()}
-            <link href="${style}" rel="stylesheet">
-        </head>
-        <body>
+        <!doctype html>
+        <html>
+          <head ${helmet.htmlAttributes.toString()}>
+            ${helmet.title.toString()}
+            ${helmet.meta.toString()}
+            ${helmet.link.toString()}
+            ${style}
+          </head>
+          <body>
             <div id="content">
-                ${body}
-                <script>
-                    window.__STATE__ = ${JSON.stringify(store.getState()).replace(/</g, '\\u003c')}
-                 </script>
-                ${scripts.join('')}
+              ${body}
             </div>
-        </body>
-    </html>
-`;
+            <script>
+              window.__STATE__ = ${state}
+            </script>
+            ${scripts}
+            ${helmet.noscript.toString()}
+          </body>
+        </html>
+      `;
       res.setHeader('Content-Type', 'text/html');
       res.end(html);
 
@@ -85,3 +83,17 @@ app.use((req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function parseBuiltTemplate() {
+  const template = readFileSync('build/index.html', 'utf8');
+  const scriptsRgx = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
+  const style = /<link href="(.*)" rel="stylesheet">/.exec(template)[0];
+  const scripts = [];
+  let out;
+
+  while ((out = scriptsRgx.exec(template)) != null) {
+    scripts.push(out[0]);
+  }
+
+  return { style, scripts: scripts.join('\n') };
+}
